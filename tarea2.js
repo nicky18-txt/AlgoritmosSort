198fe1acf50c8c81e7a048ddf699af9bfe470491
nicky18-txt <nic.altamirano@unitec.edu>
2026-05-30 06:50:45 -0600

    Insertion sort y selection sort

--- tarea2.js
+++ tarea2.js
@@ -1,24 +1,24 @@
 const array = [5, 1, 4, 2, 8]
-function bubble_sort(array){
+function bubble_sort(array) {
     let n = array.length;
 
-    if (n <=1){
+    if (n <= 1) {
         return "Ya esta ordenado"
     }
 
     let swap;
 
-    for (let i = 0; i < n; i++){
+    for (let i = 0; i < n; i++) {
         swap = false
-        for (let j = 0; j < n - 1 - i; j++){
+        for (let j = 0; j < n - 1 - i; j++) {
             let temp = array[j]
-            if (array[j] > array[j+1]){
-                array[j] = array[j+1]
-                array[j+1]=temp
-                swap=true
+            if (array[j] > array[j + 1]) {
+                array[j] = array[j + 1]
+                array[j + 1] = temp
+                swap = true
             }
         }
-        if (!swap){
+        if (!swap) {
             break;
         }
     }
@@ -26,3 +26,49 @@ function bubble_sort(array){
 }
 
 console.log(bubble_sort(array))
+
+function insertion_sort(array) {
+    let n = array.length
+    if (n <= 1) {
+        return "Ya esta ordenado el array"
+    }
+
+    for (let i = 1; i < n; i++) {
+        const k = array[i]
+        let j = i - 1
+
+        while (j >= 0 && array[j] > k) {
+            array[j + 1] = array[j]
+            j--
+        }
+        array[j + 1] = k
+    }
+    return "Array ordenado: " + array
+}
+console.log(insertion_sort(array))
+
+function selection_sort(array) {
+    let n = array.length
+
+    if (n <= 1) {
+        return "Ya esta ordenado"
+    }
+
+    for (let i = 0; i < n; i++) {
+        let min = i;
+
+        for (let j = i + 1; j < n; j++) {
+            if (array[j] < array[min]) {
+                min = j
+            }
+
+            temp = array[i]
+            array[i] = array[min]
+            array[min]= temp
+        }
+    }
+
+    return "Array ordenado: " + array
+}
+
+console.log(selection_sort(array))
